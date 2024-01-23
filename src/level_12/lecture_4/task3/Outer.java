f51a3b5432ae191835283269b4606d75c693e708
Stas <shadrin060388@gmail.com>
2024-01-23 22:33:40 +0300

    Solution lvl 12_4_3

---
+++ src/level_12/lecture_4/task3/Outer.java
@@ -0,0 +1,14 @@
+package level_12.lecture_4.task3;
+
+public class Outer {
+    class Inner {
+        Inner() {
+            System.out.println("Создание объекта внутреннего класса");
+        }
+    }
+    static class Nested {
+        Nested() {
+            System.out.println("Создание объекта вложенного класса");
+        }
+    }
+}
---
+++ src/level_12/lecture_4/task3/Solution.java
@@ -0,0 +1,8 @@
+package level_12.lecture_4.task3;
+
+public class Solution {
+    public static void main(String[] args) {
+        Outer.Inner inner = new Outer(). new Inner();
+        Outer.Nested nested = new Outer.Nested();
+    }
+}
