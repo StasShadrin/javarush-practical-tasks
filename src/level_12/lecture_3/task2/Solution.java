7e1962ccd2aa82f5e1f6317781d67b1e9cb12bdc
Stas <shadrin060388@gmail.com>
2024-01-23 16:18:11 +0300

    Update Solution lvl 12_3_2

---
+++ src/level_12/lecture_3/task2/Solution.java
@@ -0,0 +1,12 @@
+package level_12.lecture_3.task2;
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.printSize();
+        window.changeSize(4, 5);
+        window.printSize();
+    }
+}
