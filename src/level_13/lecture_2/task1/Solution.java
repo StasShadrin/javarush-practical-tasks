831103ccd772392a91ece75e50aa2459e5f8c23b
Stas <shadrin060388@gmail.com>
2024-01-24 18:26:14 +0300

    Solution lvl 13_2_1

---
+++ src/level_13/lecture_2/task1/Solution.java
@@ -0,0 +1,17 @@
+package level_13.lecture_2.task1;
+
+public class Solution {
+    public static Integer balance = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        String bill = "1234567890";
+
+        System.out.println("Текущий баланс : " + balance);
+        processPayment(bill);
+        System.out.println("Текущий баланс : " + balance);
+    }
+
+    public static void processPayment(String bill) {
+        balance -= Integer.parseInt(bill);
+    }
+}
