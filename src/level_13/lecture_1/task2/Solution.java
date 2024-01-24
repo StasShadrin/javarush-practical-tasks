4f2e399e7bb74dd542a823d35feaa31f0e977beb
Stas <shadrin060388@gmail.com>
2024-01-24 15:10:23 +0300

    Solution lvl 13_1_2

---
+++ src/level_13/lecture_1/task2/Solution.java
@@ -0,0 +1,25 @@
+package level_13.lecture_1.task2;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    Byte byteValueBox;
+    byte byteValue = byteValueBox.byteValue();
+    Short shortValueBox;
+    short shortValue = shortValueBox.shortValue();
+    Integer integerValueBox;
+    int intValue = integerValueBox.intValue();
+    Long longValueBox;
+    long longValue = longValueBox.longValue();
+
+    Float floatValueBox;
+    float floatValue = floatValueBox.floatValue();
+    Double doubleValueBox;
+    double doubleValue = doubleValueBox.doubleValue();
+
+    Character characterValueBox;
+    char charValue = characterValueBox.charValue();
+    Boolean booleanValueBox;
+    boolean booleanValue = booleanValueBox.booleanValue();
+}
