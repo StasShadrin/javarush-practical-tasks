d74460ffe8319ffb13337ee5a4fe606c2d0a5f63
Stas <shadrin060388@gmail.com>
2024-01-31 18:50:39 +0300

    Solution lvl 13_2_2

---
+++ src/level_13/lecture_2/task2/Solution.java
@@ -0,0 +1,17 @@
+package level_13.lecture_2.task2;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divide(1, 0);
+        divide(-1, 0);
+        divide(0, 0);
+        divide(100, 20);
+        divide(200, 20);
+    }
+
+    public static void divide(double a, double b) {
+        System.out.println(a / b);
+
+    }
+}
