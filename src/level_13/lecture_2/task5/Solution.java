2d1a5da2697eb9ace8c57c9f6784e0dafc3a99b8
Stas <shadrin060388@gmail.com>
2024-02-01 17:51:40 +0300

    Solution lvl 13_2_5

---
+++ src/level_13/lecture_2/task5/Solution.java
@@ -0,0 +1,22 @@
+package level_13.lecture_2.task5;
+
+public class Solution {
+    public static void main(String[] args) {
+        compare('a', 'a');
+        compare('a', 'б');
+        compare('р', 'в');
+        compare('ы', 'ы');
+    }
+
+    public static void compare(Character first, Character second) {
+        if (first.equals(second)) {
+            System.out.println("равны");
+        } else if (first > second) {
+            System.out.println("больше");
+        } else if (first < second) {
+            System.out.println("меньше");
+        } else {
+            System.out.println("А как такое может быть???");
+        }
+    }
+}
