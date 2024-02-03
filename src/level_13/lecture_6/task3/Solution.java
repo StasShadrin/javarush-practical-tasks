14a47149915d38a813ca59faa287fa12e26b226c
Stas <shadrin060388@gmail.com>
2024-02-03 19:29:45 +0300

    Solution lvl 13_6_3

---
+++ src/level_13/lecture_6/task3/Solution.java
@@ -0,0 +1,17 @@
+package level_13.lecture_6.task3;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        var exceptionsList = new ArrayList<Exception>();
+        var filesStack = new Stack<File>();
+    }
+}
