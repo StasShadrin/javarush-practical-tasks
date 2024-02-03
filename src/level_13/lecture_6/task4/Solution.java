c393bb62d4085ce75e0a9b1c5ca02df751e5e416
Stas <shadrin060388@gmail.com>
2024-02-03 19:39:17 +0300

    Solution lvl 13_6_4

---
+++ src/level_13/lecture_6/task4/Solution.java
@@ -0,0 +1,15 @@
+package level_13.lecture_6.task4;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
