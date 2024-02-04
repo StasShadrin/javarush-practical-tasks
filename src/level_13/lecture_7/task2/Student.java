3986c7fea7d2cf33eb38648308be91a7db96de31
Stas <shadrin060388@gmail.com>
2024-02-04 16:26:49 +0300

    Solution lvl 13_7_2

---
+++ src/level_13/lecture_7/task2/Student.java
@@ -0,0 +1,13 @@
+package level_13.lecture_7.task2;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
