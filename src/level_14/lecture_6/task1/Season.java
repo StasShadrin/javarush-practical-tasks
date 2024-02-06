9ced4d7eb97f9c0f75d6f2b5d698056c820fc78e
Stas <shadrin060388@gmail.com>
2024-02-06 20:04:55 +0300

    Solution lvl 14_6_1

---
+++ src/level_14/lecture_6/task1/Season.java
@@ -0,0 +1,8 @@
+package level_14.lecture_6.task1;
+
+public enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+}
---
+++ src/level_14/lecture_6/task1/Solution.java
@@ -0,0 +1,10 @@
+package level_14.lecture_6.task1;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Season.WINTER);
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+    }
+}
