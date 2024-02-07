7a0a8277702701b6d638ba38b64c75ec60263514
Stas <shadrin060388@gmail.com>
2024-02-07 13:27:23 +0300

    Solution lvl 14_6_5

---
+++ src/level_14/lecture_6/task5/Month.java
@@ -0,0 +1,32 @@
+package level_14.lecture_6.task5;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+
+    public static Month[] getWinterMonths() {
+        return new Month[]{DECEMBER, JANUARY, FEBRUARY};
+    }
+
+    public static Month[] getSpringMonths() {
+        return new Month[]{MARCH, APRIL, MAY};
+    }
+
+    public static Month[] getSummerMonths() {
+        return new Month[]{JUNE, JULY, AUGUST};
+    }
+
+    public static Month[] getAutumnMonths() {
+        return new Month[]{SEPTEMBER, OCTOBER, NOVEMBER};
+    }
+}
