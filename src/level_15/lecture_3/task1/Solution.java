10958eccc985d843777e6a0933e82f7a26b13fff
Stas <shadrin060388@gmail.com>
2024-02-10 20:57:58 +0300

    Solution lvl 15_3_1

---
+++ src/level_15/lecture_3/task1/Solution.java
@@ -0,0 +1,30 @@
+package level_15.lecture_3.task1;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        buyElephant(null, scanner);
+    }
+
+    static void buyElephant(String answer, Scanner scanner) {
+        if (answer == null) {
+            System.out.println("Купи слона");
+        } else if (answer.equalsIgnoreCase("ок")) {
+            System.out.println("Так-то лучше :) Список твоих отговорок:");
+            throw new SecurityException();
+        } else {
+            System.out.println("Все говорят \"" + answer + "\", а ты купи слона");
+        }
+
+        answer = scanner.nextLine();
+
+        try {
+            buyElephant(answer, scanner);
+        } catch (Exception e) {
+            System.out.println(answer);
+            throw e;
+        }
+    }
+}
