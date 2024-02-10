8e7376d15525b6f3b185f898d9d9b67c936a50ad
Stas <shadrin060388@gmail.com>
2024-02-10 21:28:52 +0300

    Solution lvl 18_2_1

---
+++ src/level_18/lecture_2/task1/Bat.java
@@ -0,0 +1,7 @@
+package level_18.lecture_2.task1;
+
+public class Bat extends Animal{
+    public void move(){
+        System.out.println("Я лечу!");
+    }
+}
---
+++ src/level_18/lecture_2/task1/Solution.java
@@ -0,0 +1,8 @@
+package level_18.lecture_2.task1;
+
+public class Solution {
+    public static void main(String[] args) {
+        Bat bat = new Bat();
+        bat.move();
+    }
+}
