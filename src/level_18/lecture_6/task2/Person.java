eb66de5ccf6a1150da7a830c24064d74a666a416
Stas <shadrin060388@gmail.com>
2024-02-11 19:04:30 +0300

    Solution lvl 18_6_2

---
+++ src/level_18/lecture_6/task2/Person.java
@@ -0,0 +1,24 @@
+package level_18.lecture_6.task2;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+
+    }
+}
+
