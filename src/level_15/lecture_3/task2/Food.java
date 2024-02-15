462619439eb911cc73738ed9ac6744800bd40b16
Stas <shadrin060388@gmail.com>
2024-02-15 21:59:06 +0300

    Solution lvl 15_3_2

---
+++ src/level_15/lecture_3/task2/Food.java
@@ -0,0 +1,9 @@
+package level_15.lecture_3.task2;
+
+public class Food {
+    String name;
+
+    public Food(String name) {
+        this.name = name;
+    }
+}
---
+++ src/level_15/lecture_3/task2/Solution.java
@@ -0,0 +1,9 @@
+package level_15.lecture_3.task2;
+
+public class Solution {
+    public static void main(String[] args) {
+        Lion lion = new Lion();
+        lion.eat(new Food("мясо"));
+        lion.eat(null);
+    }
+}
