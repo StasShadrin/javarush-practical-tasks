4008435db7bdae3552d1115410e78393024d4b79
Stas <shadrin060388@gmail.com>
2024-02-16 18:59:07 +0300

    Solution lvl 15_5_3

---
+++ src/level_15/lecture_5/task3/MyStack.java
@@ -0,0 +1,28 @@
+package level_15.lecture_5.task3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyStack {
+    private final List<String> storage = new LinkedList<>();
+
+    public void push(String s) {
+        storage.add(0, s);
+    }
+
+    public String pop() {
+        return storage.remove(0);
+    }
+
+    public String peek() {
+        return storage.get(0);
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        return storage.indexOf(s);
+    }
+}
