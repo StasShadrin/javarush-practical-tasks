b5b870139ef93ae7a5a7f1542517259d5179d193
Stas <shadrin060388@gmail.com>
2024-02-16 18:49:43 +0300

    Solution lvl 15_5_2

---
+++ src/level_15/lecture_5/task2/Solution.java
@@ -0,0 +1,28 @@
+package level_15.lecture_5.task2;
+
+public class Solution {
+    public static final String OUTPUT_FORMAT = "Метод %s вызван из строки %d класса %s в файле %s.\n";
+
+    public static void main(String[] args) {
+        makeScrewdriver();
+    }
+
+    public static void printStackTrace(StackTraceElement[] stackTrace) {
+
+        for (StackTraceElement info : stackTrace) {
+            System.out.printf(OUTPUT_FORMAT, info.getMethodName(), info.getLineNumber(), info.getClassName(), info.getFileName());
+        }
+    }
+
+    static void makeScrewdriver() {
+        addJuice();
+    }
+
+    static void addJuice() {
+        addVodka();
+    }
+
+    static void addVodka() {
+        printStackTrace(Thread.currentThread().getStackTrace());
+    }
+}
