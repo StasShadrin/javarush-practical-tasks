bf42f537e73db3de461d742b109490dfc1d19653
Stas <shadrin060388@gmail.com>
2024-02-19 13:48:25 +0300

    Solution lvl 16_1_1

---
+++ src/level_16/lecture_1/task1/Solution.java
@@ -0,0 +1,16 @@
+package level_16.lecture_1.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = reader.readLine();
+            System.out.println(line.toLowerCase());
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
