e289d37425ef500a800c786d4c016bb2103b1fa6
Stas <shadrin060388@gmail.com>
2024-02-21 17:45:50 +0300

    Solution lvl 16_6_3

---
+++ src/level_16/lecture_6/task3/Solution.java
@@ -0,0 +1,27 @@
+package level_16.lecture_6.task3;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution {
+
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path directory = Path.of(scanner.nextLine());
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(directory)) {
+            for (Path path : files) {
+                if (Files.isRegularFile(path)) {
+                    System.out.println(path + THIS_IS_FILE);
+                } else if (Files.isDirectory(path)) {
+                    System.out.println(path + THIS_IS_DIR);
+                }
+            }
+        }
+    }
+}
