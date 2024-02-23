30815eac23f9aaf1d331e57240e917dcefcafd16
Stas <shadrin060388@gmail.com>
2024-02-23 16:54:32 +0300

    Solution lvl 17_4_3

---
+++ src/level_17/lecture_4/task3/Solution.java
@@ -0,0 +1,13 @@
+package level_17.lecture_4.task3;
+
+import java.time.LocalDate;
+
+public class Solution {
+    public static void main(String[] args) {
+        LocalDate jubilee40Years = LocalDate.of(2010, 3, 13);
+
+        LocalDate jubilee30Years = jubilee40Years.minusYears(10);
+
+        System.out.println(Math.abs(jubilee30Years.getYear()));
+    }
+}
