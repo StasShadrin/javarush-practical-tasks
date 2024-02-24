4f20c7dad127a6b514bc3cf33881e659ee62a74e
Stas <shadrin060388@gmail.com>
2024-02-24 19:59:10 +0300

    Solution lvl 17_7_1

---
+++ src/level_17/lecture_7/task1/Solution.java
@@ -0,0 +1,27 @@
+package level_17.lecture_7.task1;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.TreeSet;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        TreeSet<String> sortedZones = getSortedZones();
+        System.out.println(sortedZones.size());
+        System.out.println(sortedZones.first());
+        System.out.println(sortedZones.last());
+
+        System.out.println(getBeijingDateTime());
+    }
+
+    static TreeSet<String> getSortedZones() {
+        return new TreeSet<>(ZoneId.getAvailableZoneIds());
+    }
+
+    static ZonedDateTime getBeijingDateTime() {
+        ZoneId zone = ZoneId.of("Asia/Shanghai");
+        return ZonedDateTime.now(zone);
+    }
+}
+
