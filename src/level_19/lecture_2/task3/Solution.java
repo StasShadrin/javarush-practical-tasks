243b3b96370aa53d4fbde825ef628ed549c6b2b1
Stas <shadrin060388@gmail.com>
2024-02-28 18:38:48 +0300

    Solution lvl 19_2_3

---
+++ src/level_19/lecture_2/task3/Solution.java
@@ -0,0 +1,17 @@
+package level_19.lecture_2.task3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+        Collections.addAll(strings, "Hello", "Amigo", "JavaRush", "CodeGym");
+
+        print(strings);
+    }
+
+    public static void print(ArrayList<String> strings) {
+        strings.forEach(System.out::println);
+    }
+}
