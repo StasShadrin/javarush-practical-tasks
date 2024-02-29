b93f56e516a299d9db2d07c6e634f1d94ff2f9af
Stas <shadrin060388@gmail.com>
2024-02-29 21:46:50 +0300

    Solution lvl 19_4_1

---
+++ src/level_19/lecture_4/task1/Language.java
@@ -0,0 +1,24 @@
+package level_19.lecture_4.task1;
+
+public class Language {
+    private String name;
+    private Double ranking;
+
+    public Language(String name, Double ranking) {
+        this.name = name;
+        this.ranking = ranking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getRanking() {
+        return ranking;
+    }
+
+    @Override
+    public String toString() {
+        return "Язык программирования - " + name + ", рейтинг  - " + ranking + "% опрошенных.";
+    }
+}
