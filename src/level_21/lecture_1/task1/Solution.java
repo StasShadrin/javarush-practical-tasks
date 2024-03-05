1d592dffb0ad9a1f99841f965aadfdb1f198e737
Stas <shadrin060388@gmail.com>
2024-03-05 13:15:11 +0300

    Solution lvl 21_1_1

---
+++ src/level_21/lecture_1/task1/Solution.java
@@ -0,0 +1,39 @@
+package level_21.lecture_1.task1;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+        int min = 2147483647;
+        int max = -2147483648;
+        for (int j : inputArray) {
+            if (j < min) {
+                min = j;
+            }
+            if (j > max) {
+                max = j;
+            }
+        }
+        return new Pair(min, max);
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
