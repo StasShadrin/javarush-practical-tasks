52c64539f47c50e617de2122ce0e97203aba4a11
Stas <shadrin060388@gmail.com>
2024-03-12 21:14:59 +0300

    Solution lvl 23_2_1

---
+++ src/level_23/lecture_2/task1/Solution.java
@@ -0,0 +1,14 @@
+package level_23.lecture_2.task1;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Person {
+        boolean isAlive();
+    }
+
+    interface Presentable extends Person {
+
+    }
+}
